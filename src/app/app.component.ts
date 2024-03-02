e783f664fe9604ab406196988095c1b45dad18c1
isra997 <isra_su@outlook.com>
2024-03-02 15:00:16 -0500

    Tercer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TombolaComponent } from './tombola/tombola.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TombolaComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
